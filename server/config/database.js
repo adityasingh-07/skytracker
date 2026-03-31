a3ce0d22c5e0b886d7b6577cf4e0dc361f8c273d
gajodhariscool <codelazar69966996@gmail.com>
2026-04-01 02:24:19 +0530

    Add SSL support for cloud MySQL

--- server/config/database.js
+++ server/config/database.js
@@ -10,6 +10,12 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: 'mysql',
     logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
     pool: {
       max: 10,
       min: 0,
